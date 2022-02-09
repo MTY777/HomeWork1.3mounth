e8c48909e22fdcd1de7daf909f989ede4544376b
MTY777 <mataymekkurmanbek@gmail.com>
2022-02-10 01:14:45 +0600

    t

---
+++ app/src/main/java/com/example/homework13mounth/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.homework13mounth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
